ef84885cfa330c6936f571f92cfce5dcf9807887
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-01-27 08:51:38 -0500

    add color options to product options

--- client/components/Favorite.jsx
+++ client/components/Favorite.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { FaRegHeart } from "react-icons/fa";
 
 const Favorite = () => {
   return (
-    <button className="w-full mt-3 py-4 border border-gray-300 rounded-full hover:border-black">
+    <button className="flex flex-row justify-center w-full mt-3 py-4 border border-gray-300 rounded-full hover:border-black">
       Favorite
+      <FaRegHeart className="mt-1 ml-3" />
     </button>
   );
 };
--- client/components/ProductOptions.jsx
+++ client/components/ProductOptions.jsx
@@ -5,6 +5,7 @@ import ShoeSizes from "./ShoeSizes";
 import Klarna from "./Klarna";
 import AddToBag from "./AddToBag";
 import Favorite from "./Favorite";
+import Colors from "./Colors";
 
 const ProductOptions = () => {
   const [shoe, setShoe] = useRecoilState(shoeState);
@@ -13,7 +14,8 @@ const ProductOptions = () => {
     <div className="m-2 font-sans w-96">
       <h1 className="pb-2 font-medium tracking-[0.007em] text-[28px] leading-[1.2rem]">{shoe.name}</h1>
       <h2 className="pb-4 font-medium text-base leading-6">{shoe.category}</h2>
-      <h2 className="font-medium">${shoe.price}</h2>
+      <h2 className="mt-2 font-medium">${shoe.price}</h2>
+      <Colors />
       <ShoeSizes />
       <Klarna price={shoe.price}/>
       <AddToBag />

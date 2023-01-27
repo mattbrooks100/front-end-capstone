5ecb07f270edc54a2d6db177259ac9230b47a32e
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-01-27 10:08:35 -0500

    styling to social media icons in Footer

--- client/components/Footer/SocialMedia.jsx
+++ client/components/Footer/SocialMedia.jsx
@@ -3,13 +3,19 @@ import { FaTwitter, FaFacebookF, FaYoutube, FaInstagram } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
-    <div className="flex flex-row my-10">
-      <button className="grid place-content-center bg-gray-400 h-[30px] w-[30px] rounded-full">
+    <div className="flex flex-row my-10 gap-x-4">
+      <button className="grid place-content-center bg-gray-500 h-[30px] w-[30px] rounded-full">
         <FaTwitter className="invert" />
       </button>
-      <FaFacebookF />
-      <FaYoutube />
-      <FaInstagram />
+      <button className="grid place-content-center bg-gray-500 h-[30px] w-[30px] rounded-full">
+        <FaFacebookF className="invert" />
+      </button>
+      <button className="grid place-content-center bg-gray-500 h-[30px] w-[30px] rounded-full">
+        <FaYoutube className="invert" />
+      </button>
+      <button className="grid place-content-center bg-gray-500 h-[30px] w-[30px] rounded-full">
+        <FaInstagram className="invert" />
+      </button>
     </div>
   );
 };

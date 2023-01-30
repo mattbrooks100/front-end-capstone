7fbbefec7d8ca39feaf62498329ef8587e9a33ce
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-01-30 15:42:11 -0500

    update Info section styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -23,14 +23,16 @@ const App = () => {
       <header className="flex h-[100px] bg-gray-300 font-bold justify-center">
         HEADER GOES HERE
       </header>
-      <div className="grid grid-flow-col auto-cols-auto gap-16 mt-8 px-8 w-full justify-center">
+      <div className="grid grid-cols-2 gap-16 mt-8 px-8 w-full justify-center">
         <ProductPhotos />
-        <ProductOptions />
+        <div>
+          <ProductOptions />
+          <Info />
+        </div>
       </div>
       <div className="mt-16 mx-8">
         <Carousel />
       </div>
-      <Info />
       <Explore />
       <Footer />
     </RecoilRoot>

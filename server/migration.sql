6e76fee39d874f7deb5f968e4e5266c3fffadbfe
Jersen Reyes <jersen222@gmail.com>
2023-01-31 10:00:42 -0800

    changed info to match actual shoes

--- server/migration.sql
+++ server/migration.sql
@@ -9,4 +9,4 @@ CREATE TABLE shoes (
   sizes NUMERIC[]
 );
 
-INSERT INTO shoes (name, category, price, color, sizes) VALUES ('Nike Air Force 1 ''07', 'Men''s Shoes', 110, '{White,Black,Design Your Own}', '{6,6.5,7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12,12.5,13,14,15,16,17,18}');
+INSERT INTO shoes (name, category, price, color, sizes) VALUES ('Nike Dunk Low ''Medium Curry', 'Men''s Shoes', 110, '{White,Black,Design Your Own}', '{6,6.5,7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12,12.5,13,14,15,16,17,18}');

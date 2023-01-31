3ee9d314a740be6076732e113f8c4ec29789ba2d
sds3000 <64442811+sds3000@users.noreply.github.com>
2023-01-31 13:23:29 -0500

    Reviews component fix

--- client/components/reviews.jsx
+++ client/components/reviews.jsx
@@ -1,48 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dropdown } from "./dropdown";
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
 
 
 const Reviews = (props) => {
-
-    // create button
-    // no borders and bg-color same as page
-
+    const [ showReview, setShowReview] = useState(false)
     return (
-        <div className="group inline-block">
-            <div
-                className="outline-none focus:outline-none bg-white rounded-sm flex items-center min-w-32"
-            >
-                <span className="pr-1 font-bold flex-1">
-                    Reviews
-                </span>
-                <span>
-                <svg
-                width={15}
-                className=" h-4 w-4 transform group-hover:-rotate-180 transition duration-150 ease-in-out"
-                xmlns="http://www.w3.org/z2000/svg"
-                viewBox="20 20 20 20"
-                >
-                    <path
-                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                    />
-                </svg>
-                </span>
-            </div>
-            <Dropdown />
-            <hr />
-            <summary className="relative">
-                <h3><span>Reviews</span></h3>
-            </summary>
-            <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
-                <div class="rounded-md bg-white shadow-xs">
-                    
-             </div>
-
-            </div>
-            </div>
-        
-        
-        
+        <div className="py-8 border-b border-gray-200">
+      <button className="flex w-full justify-between" onClick={() => setShowReview(!showReview)}>
+        <div className="text-xl">Reviews</div>
+        {showReview ? (
+          <MdOutlineKeyboardArrowUp size={25} />
+        ) : (
+          <MdOutlineKeyboardArrowDown size={25} />
+        )}
+      </button>
+      {showReview && (
+        <div className="mt-8">
+          <button className="font-semibold underline ml-1">Write a review</button>
+          <Dropdown />
+          <button className="font-semibold underline ml-1">More Reviews</button>
+          
+        </div>
+      )}
+    </div>        
     )
 }
 

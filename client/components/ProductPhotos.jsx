b6bfd892d9508767caed5b38e9ee9567f6ec23f2
Jersen Reyes <jersen222@gmail.com>
2023-02-09 13:47:38 -0800

    compressed gif file

--- client/components/ProductPhotos.jsx
+++ client/components/ProductPhotos.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import Dunks1 from "../photos/Dunks1.jpg";
-import JersenDunks from "../photos/jersendunks_v2.gif";
+import JersenDunks from "../photos/jersendunks_v2.webm";
 import Dunks2 from "../photos/Dunks2.webp";
 import Dunks3 from "../photos/Dunks3.webp";
 import Dunks4 from "../photos/Dunks4.webp";
@@ -8,13 +8,33 @@ import Dunks5 from "../photos/Dunks5.webp";
 import Dunks6 from "../photos/Dunks6.webp";
 
 const ProductPhotos = () => {
+
+  const videoEl = useRef(null);
+  const attemptPlay = () => {
+    videoEl &&
+      videoEl.current &&
+      videoEl.current.play().catch(error => {
+        console.error("Error attempting to play", error);
+      });
+  };
+
+  useEffect(() => {
+    attemptPlay();
+  }, []);
+
   return (
     <div className="grid grid-cols-2 gap-4 h-fit">
       <div>
         <img src={Dunks1}></img>
       </div>
       <div>
-        <img src={JersenDunks}></img>
+        <video 
+          playsInline
+          loop
+          muted
+          src={JersenDunks}
+          ref={videoEl}/>
+        
       </div>
       <div>
         <img src={Dunks2}></img>

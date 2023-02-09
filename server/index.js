82bf299fc930c4f5fb0648ff587448a530d9c6e8
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-02-09 16:59:42 -0500

    added redis and redis client caching in server index.js file

--- server/index.js
+++ server/index.js
@@ -1,37 +1,60 @@
 import express from "express";
 import cors from "cors";
 import postgres from "postgres";
+import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
-
 const sql = postgres(process.env.DATABASE_URL);
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/shoes", (req, res) => {
-  sql`SELECT * FROM shoes`.then((rows) => {
-    res.send(rows);
+// REDIS IMPLEMENTATION FOR CLIENT-SIDE CACHING
+import redis from "redis";
+let redisClient;
+(async () => {
+  console.log("Starting Redis...");
+  redisClient = redis.createClient();
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+  await redisClient.connect();
+  console.log("Redis connected.");
+})();
+
+async function getShoes(req, res) {
+  try {
+    console.log("Checking for cached data...");
+    const shoeData = await redisClient.get("shoes");
+    if (shoeData) {
+      console.log("Cached data found.")
+      res.send(shoeData);
+    } else {
+      console.log("No cached data. Querying database...");
+      const shoeData = await sql`SELECT * FROM shoes`;
+      res.send(JSON.stringify(shoeData));
+      console.log("Saving data in cache.")
+      redisClient.set("shoes", JSON.stringify(shoeData));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+// END REDIS IMPLEMENTATION
+
+app.get("/api/shoes", getShoes);
+
+app.get("/api/reviews", (req, res) => {
+  sql`SELECT * FROM reviews`.then((review) => {
+    res.send(review);
   });
 });
 
-app.get("/api/reviews", (req, res) => {
-  sql`SELECT * FROM reviews`.then(review => {
-    res.send(review)
-  })
-})
-
-app.post("/api/reviews", (req,res) => {
-  let review = req.body; 
-    // let {username, title, body} = review
-    sql`INSERT INTO reviews ${sql(review)} RETURNING *`.then((result) => {
-      res.json(result)
-    })
-})
+app.post("/api/reviews", (req, res) => {
+  let review = req.body;
+  sql`INSERT INTO reviews ${sql(review)} RETURNING *`.then((result) => {
+    res.json(result);
+  });
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
-// (username, title, body) VALUES(${username, title, body})

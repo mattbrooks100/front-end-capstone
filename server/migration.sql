7e33e3e772a847c3479cdfdbd9dd8bcbde01cadd
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-02-09 17:43:35 -0500

    added 1,000,000 records to shoe db

--- server/migration.sql
+++ server/migration.sql
@@ -22,14 +22,12 @@ CREATE TABLE reviews (
   amount INTEGER
 );
 
-
-
-
-/*
-change name to actual show name, change color to Tan/White,
-*/
 INSERT INTO shoes (name, category, price, color, sizes) VALUES ('Nike Dunk Low ''Medium Curry''', 'Men''s Shoes', 110, '{Tan/White}', '{6,6.5,7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12,12.5,13,14,15,16,17,18}'),
 ('Nike Air Force 1', 'Men''s Shoes', 130, '{White,Black,Design Your Own}', '{6,6.5,7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12,12.5,13,14,15,16,17,18}');
+
+INSERT INTO shoes (name, category, price, color, sizes)
+SELECT substr(md5(random()::text), 1, 10), 'Men''s Shoes', (random() * 100 + 65)::integer, '{White,Black}', '{6,6.5,7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12,12.5,13,14,15,16,17,18}'
+FROM generate_series(1, 1000000);
 
 INSERT INTO reviews(shoes_id, title, date, username, body, rating) VALUES('1','I can walk!', 'Feb 01, 2023', 'BigSteppa', 'I could walk before but now I can walk in style.', 5), 
 ('1','The tan not tanning', 'Jan 12, 2023', 'BlackAirForceKi','Going back to black forces so I can terrorize basketball courts.', 1),
